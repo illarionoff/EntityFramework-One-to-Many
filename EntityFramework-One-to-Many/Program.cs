6e76232149a1409d1c256eb27749ed12b1b4259b
Afanasiy Illarionov <afanasiy.illarionov@dataart.com>
2020-02-13 14:14:33 +0300

    Add project files.

---
+++ EntityFramework-One-to-Many/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFramework_One_to_Many
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
